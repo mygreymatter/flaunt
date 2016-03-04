6af696591d3d311e62736e40b1805ffe7f24fb17
Mahayogi Lakshmipathi <mahayogi@scimata.com>
2016-03-04 14:07:50 +0530

    implemented the launch page

--- public/js/modules/home.js
+++ public/js/modules/home.js
@@ -1,7 +1,24 @@
 (function () {
     angular.module('HomeModule', [])
-        .controller('HomeController', function ($scope) {
+        .controller('HomeController', function ($scope, $interval) {
 
-            $scope.name = 'Chic';
+            var a = angular.element(document.getElementById('a')),
+                b = angular.element(document.getElementById('b')),
+                c = angular.element(document.getElementById('c')),
+                d = angular.element(document.getElementById('d')),
+                src = "../images/";
+
+            $scope.slide_image = src + "rose_smells_rose" + ".jpg";
+            console.log('Slide Image: ' + $scope.slide_image);
+
+            $scope.name = 'Collections';
+            $scope.rows = [{
+                'images': [1, 2, 3, 4]
+            }, {
+                'images': [1, 2, 3, 4]
+            }];
+            $scope.img_name = "saree_1";
+            $scope.store = "store_2";
+
         });
 })();
