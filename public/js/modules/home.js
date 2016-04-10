9cc924095d56f1f9c1c5166a156b5b6b1d68a36b
Mahayogi Lakshmipathi <mahayogi@scimata.com>
2016-04-10 22:18:17 +0530

    deployed to heroku

--- public/js/modules/home.js
+++ public/js/modules/home.js
@@ -1,80 +1,51 @@
 (function () {
-    angular.module('HomeModule', [])
-        .controller('HomeController', function ($scope, $interval) {
+    angular.module('HomeModule', ['Products'])
+        .controller('HomeController', ['$scope', '$state', '$interval', 'products',
+                                       function ($scope, $state, $interval, products) {
+                console.log('Products: ' + products[0].image);
 
-            console.log('Home Width: ' + screen.width);
-            var a = angular.element(document.getElementById('a')),
-                b = angular.element(document.getElementById('b')),
-                c = angular.element(document.getElementById('c')),
-                d = angular.element(document.getElementById('d')),
-                src = "../images/screens/";
+                $scope.products = products;
 
-            $scope.slide_image = src + "rose_smells_rose" + ".jpg";
-            $scope.widthClass = screen.width < 400 ? "col-xs-12 col-sm-6 col-md-3 item" :
-                "col-xs-6 col-sm-6 col-md-3 item";
-            $scope.name = 'Collections';
+                var a = angular.element(document.getElementById('a')),
+                    b = angular.element(document.getElementById('b')),
+                    c = angular.element(document.getElementById('c')),
+                    d = angular.element(document.getElementById('d')),
+                    src = "../images/screens/";
 
-            $scope.collections = [{
-                'image': 'screen-shop1',
-                'name': 'Dress Less',
-                'price': 1400
-                }, {
-                'image': 'screen-shop2',
-                'name': 'Jeans Dress',
-                'price': 1650
-                }, {
-                'image': 'screen-shop3',
-                'name': 'Little Red',
-                'price': 1100
-                }, {
-                'image': 'screen-shop4',
-                'name': 'Bird Song',
-                'price': 2500
-                }, {
-                'image': 'screen-shop5',
-                'name': 'Spring',
-                'price': 1500
-                }, {
-                'image': 'screen-shop6',
-                'name': 'Maya Dress',
-                'price': 1400
-                }, {
-                'image': 'screen-shop7',
-                'name': 'Water Dream',
-                'price': 1800
-                }, {
-                'image': 'screen-shop8',
-                'name': 'Branding Bug',
-                'price': 1150
-                }];
+                $scope.slide_image = src + "rose_smells_rose" + ".jpg";
+                $scope.widthClass = screen.width < 400 ? "col-xs-12 col-sm-6 col-md-3 item" :
+                    "col-xs-6 col-sm-6 col-md-3 item";
 
-
-
-            $scope.stores = [{
-                'image': 'store_2',
-                'name': 'Paisley'
+                $scope.stores = [{
+                    'image': 'store_2',
+                    'name': 'Paisley'
 	               }, {
-                'image': 'store_2',
-                'name': 'Aishwarya Design Studio'
+                    'image': 'store_2',
+                    'name': 'Aishwarya Design Studio'
 	               }, {
-                'image': 'store_2',
-                'name': 'Studio Rudraksh'
+                    'image': 'store_2',
+                    'name': 'Studio Rudraksh'
 	               }, {
-                'image': 'store_2',
-                'name': 'Vajra'
+                    'image': 'store_2',
+                    'name': 'Vajra'
 	               }, {
-                'image': 'store_2',
-                'name': 'Studio Upasana'
+                    'image': 'store_2',
+                    'name': 'Studio Upasana'
 	               }, {
-                'image': 'store_2',
-                'name': 'Smriti\'s'
+                    'image': 'store_2',
+                    'name': 'Smriti\'s'
 	               }, {
-                'image': 'store_2',
-                'name': 'Obsession'
+                    'image': 'store_2',
+                    'name': 'Obsession'
 	               }, {
-                'image': 'store_2',
-                'name': 'Banwery'
+                    'image': 'store_2',
+                    'name': 'Banwery'
 	               }];
 
-        });
+                $scope.showProduct = function () {
+                    console.log('Show Product');
+                    $state.go('Details');
+                };
+
+                        }]);
 })();
